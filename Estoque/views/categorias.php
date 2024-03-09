aae2b270deb963b39034db3aa41d144e2270df4d
Arie0 <arielykarla3@gmail.com>
2024-03-08 21:41:03 -0300

    continued

---
+++ Estoque/views/categorias.php
@@ -0,0 +1,30 @@
+<table class="table table-bordered">
+  <thead>
+    <tr>
+      <th scope="col">Compras</th>
+      <th scope="col">Mercado</th>
+      <th scope="col">Farmacia</th>
+      <th scope="col">Lazer</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Comida</td>
+      <td>Cuidados</td>
+      <td>Pet</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Limpeza</td>
+      <td>Remedios</td>
+      <td>Passeios</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Suplementos</td>
+      <td>Rosto</td>
+      <td>Cabelo</td>
+    </tr>
+  </tbody>
+</table>
